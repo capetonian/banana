3f58f3839a2c31217fd6fbbb220596e3d81bd643
Andrew La Grange <andrew.lagrange@fullstack.co.za>
2016-05-28 21:59:02 +0200

    diagnostic

--- src/app/panels/subcollection/diagnostic.js
+++ src/app/panels/subcollection/diagnostic.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+  console.log('loading diagnostic...')
+  getSolrData();
+
+
+});
+
+var fs_chartData = {
+  labels: [],
+  series: []
+};
+
+
+
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
@@ -6,4 +20,70 @@ function getParameterByName(name, url) {
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+function getSolrData() {
+  //http://hdp24.fullstack.co.za:8983/solr/social_solr/select?q=*:*&wt=json
+  $.ajax({
+    'url': 'http://hdp24.fullstack.co.za:8983/solr/social_solr/select',
+    'data': {
+      'wt': 'json',
+      'q': '*:*'
+    },
+    'success': function(data) {
+      console.log(data.response.docs);
+      for (i in data.response.docs) {
+
+        //console.log(i);
+
+        $('#content').html($('#content').html() + "<BR>" + JSON.stringify(
+          data.response.docs[i], null, 2));
+
+
+
+        if (i == 0) { // First row
+          for (x in data.response.docs[0]) {
+
+            switch (x) {
+              case '_version_':
+              case 'id':
+                // do nothing
+                break;
+              default:
+                console.log(x);
+                fs_chartData.labels.push(x);
+                break;
+            }
+
+          }
+
+        }
+
+        var tmpArray = [];
+
+        for (var key in data.response.docs[i]) {
+          if (data.response.docs[i].hasOwnProperty(key)) {
+            switch (key) {
+              case '_version_':
+              case 'id':
+                // do nothing
+                break;
+              default:
+                console.log(x);
+                tmpArray.push(data.response.docs[i][key][0]);
+                break;
+            }
+
+          }
+        }
+
+        fs_chartData.series.push(tmpArray);
+
+      }
+      console.log(fs_chartData);
+    },
+    'dataType': 'jsonp',
+    'jsonp': 'json.wrf'
+  });
 }
